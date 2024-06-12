d177698a5ba48cad77de4e8392c0daf52d16eea0
Rishi <hirishinatraj@gmail.com>
2024-06-12 14:43:54 -0500

    Basic

---
+++ my-app/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Header = () => {
+    return (
+        <header>
+            <h1>My First React App</h1>
+            <p>Welcome to my first React app!</p>
+        </header>
+    )
+  }
+  
+  export default Header
